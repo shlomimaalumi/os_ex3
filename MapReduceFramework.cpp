ca0c42e2b2d154a6b36c082bd81018094766b363
shlomimaalumi <shlomom.maalumi@mail.huji.ac.il>
2023-05-10 13:20:22 +0300

    1

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -15,8 +15,21 @@ typedef struct ThreadContext {
     pthread_mutex_t *mutex;
     std::atomic<int> *atomicCounter;
     int multiThreadLevel;
-};
+    JobState *current_state;
+    std::vector<IntermediateVec> *intermediate_vecs;
+} ThreadContext;
 
+typedef struct ShuffleContext {
+    pthread_mutex_t *mutex;
+    std::atomic<int> *atomicCounter;
+    JobState *current_state;
+    std::vector<IntermediateVec> *intermediate_vecs;
+} ShuffleContext;
+
+typedef struct WaitContext {
+    std::vector<pthread_t> *threads;
+    int multiThreadLevel;
+} WaitContext;
 
 JobState current_state;
 
@@ -38,7 +51,8 @@ void *map_phase(void *context) {
         pthread_mutex_unlock(t_context->mutex);
 
         std::pair<K1 *, V1 *> pair = (t_context->input_vec)->at(currentIndex);
-        // lock the mutex because use the input vector
+
+        // TODO: CHECK IF NEED TO lock the mutex because use the input vector
         t_context->client->map(pair.first, pair.second, (void *) t_context);
 
     }
@@ -48,6 +62,48 @@ void *map_phase(void *context) {
     pthread_mutex_unlock(t_context->mutex);
     return nullptr;
 }
+
+
+void shuffle_phase(ShuffleContext &context) {
+
+}
+
+
+void *reduce_phase(void *context) {
+    return nullptr;
+}
+
+
+//    ThreadContext *t_context = (ThreadContext *) context;
+//
+//    // Keep processing intermediate key-value pairs until all pairs have been reduced
+//    while (true) {
+//        int currentKeyIndex = t_context->atomicCounter->fetch_add(1);
+//
+//        // Check if all keys have been processed by other threads already
+//        if (currentKeyIndex >= (int) t_context->keysVec->size()) {
+//            break;
+//        }
+//
+//        K2 *key = t_context->keysVec->at(currentKeyIndex);
+//
+//        // Collect all intermediate values associated with the current key
+//        std::vector<V2 *> valuesVec;
+//        for (int i = 0; i < t_context->intermediateVecs->size(); i++) {
+//            IntermediateVec *vec = t_context->intermediateVecs->at(i);
+//            for (int j = 0; j < vec->size(); j++) {
+//                IntermediatePair *pair = &vec->at(j);
+//                if (*(pair->first) == *key) {
+//                    valuesVec.push_back(pair->second);
+//                }
+//            }
+//        }
+//
+//        // Reduce the values and emit the final key-value pair
+//        V2 *outputValue = t_context->client->reduce(key, valuesVec, t_context);
+//    }
+//    return nullptr;
+//}
 
 
 /*
@@ -86,26 +142,68 @@ JobHandle startMapReduceJob(const MapReduceClient &client,
 
 
     for (int i = 0; i < multiThreadLevel; ++i) {
-        threadContexts[i] = {&client,
-                             &inputVec,
-                             &intermediateVectors[i],
-                             &outputVec,
-                             &mutex,
-                             &atomicCounter,
-                             multiThreadLevel};
-        if (pthread_create(&threads[i], NULL, map_phase,
-                           (void *) &threadContexts[i])) {
+        map_thread_contexts[i] = {&client,
+                                  &inputVec,
+                                  &intermediateVectors[i],
+                                  nullptr,
+                                  &mutex,
+                                  &atomicCounter,
+                                  multiThreadLevel,
+                                  &current_state,
+                                  &intermediateVectors};
+        if (pthread_create(&map_threads[i], NULL, map_phase,
+                           (void *) &map_thread_contexts[i])) {
             std::cerr << "Error creating thread" << std::endl;
             exit(1);
         }
     }
 
     // Wait for the threads to finish and collect their intermediate results
-    for (int i = 0; i < multiThreadLevel; i++) {
-        pthread_join(threads[i], NULL);
+    WaitContext curr_wait = {&map_threads, multiThreadLevel};
+    waitForJob(&curr_wait);
+
+    // Update the job state to the shuffle phase
+    current_state.stage = SHUFFLE_STAGE;
+    ShuffleContext shuffle_context = {
+            &mutex,
+            &atomicCounter,
+            &current_state,
+            &intermediateVectors};
+
+    // NOW perform the shuffle phase:
+    shuffle_phase(shuffle_context);
+
+    // Update the job state to the reduce phase
+    current_state.stage = REDUCE_STAGE;
+
+    // create empty vector for all the threads
+    std::vector<pthread_t> reduce_threads(multiThreadLevel);
+
+    // an array to store all the context for each thread
+    ThreadContext reduce_threads_context[multiThreadLevel];
+
+    for (int i = 0; i < multiThreadLevel; ++i) {
+        reduce_threads_context[i] = {&client,
+                                     &inputVec,
+                                     &intermediateVectors[i],
+                                     nullptr,
+                                     &mutex,
+                                     &atomicCounter,
+                                     multiThreadLevel,
+                                     &current_state,
+                                     &intermediateVectors};
+        if (pthread_create(&reduce_threads[i], NULL, reduce_phase,
+                           (void *) &reduce_threads_context[i])) {
+            std::cerr << "Error creating thread" << std::endl;
+            exit(1);
+        }
     }
 
-    // **************** done map phase ****************
+    curr_wait = {&reduce_threads, multiThreadLevel};
+    waitForJob(&curr_wait);
+
+    current_state.stage = UNDEFINED_STAGE;
+    current_state.percentage = 100;
 
 //        Free resources
     pthread_mutex_destroy(&mutex);
@@ -153,5 +251,33 @@ void emit2(K2 *key, V2 *value, void *context) {
     pthread_mutex_lock(t_context->mutex);
     t_context->intermediate_vec->push_back(*pair);
     pthread_mutex_unlock(t_context->mutex);
+}
+
+
+void emit3(K3 *key, V3 *value, void *context) {}
+
+
+void waitForJob(JobHandle job) {
+    WaitContext *curr = (WaitContext *) job;
+    for (int i = 0; i < curr->multiThreadLevel; i++) {
+        pthread_join((*curr->threads)[i], NULL);
+    }
+}
+
+
+void getJobState(JobHandle job, JobState *state) {
+    ThreadContext *t_job = (ThreadContext *) job;
+    state->stage = t_job->current_state->stage;
+    state->percentage = t_job->current_state->percentage;
+}
+
+
+void closeJobHandle(JobHandle job) {
+    ThreadContext *t_job = (ThreadContext *) job;
+    for (auto vec: *(t_job->intermediate_vecs)) {
+        for (auto pair: vec) {
+            delete &pair;
+        }
+    }
 }
 
